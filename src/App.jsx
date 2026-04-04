349d0e9147c327f06356803c9ce072226a693457
Prachi Panwar <prachipanwar@Prachis-MacBook-Air.local>
2026-04-05 00:27:18 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-3xl font-bold text-blue-500">
+    Tailwind working
+  </h1>
+  )
+}
+
+export default App
